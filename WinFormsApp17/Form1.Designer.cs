25e2ba7eeb75c56c938fd979e25d97ba4fe92d0a
Claus <Claus@LENOVO>
2024-10-24 20:49:13 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp17/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WinFormsApp17
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdicionar = new Button();
+            btnEditar = new Button();
+            btnExcluir = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.Location = new Point(168, 76);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.Size = new Size(141, 64);
+            btnAdicionar.TabIndex = 0;
+            btnAdicionar.Text = "Adicionar";
+            btnAdicionar.UseVisualStyleBackColor = true;
+            btnAdicionar.Click += btnAdicionar_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(315, 76);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(153, 64);
+            btnEditar.TabIndex = 1;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.Location = new Point(474, 76);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(162, 64);
+            btnExcluir.TabIndex = 2;
+            btnExcluir.Text = "Excluir";
+            btnExcluir.UseVisualStyleBackColor = true;
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(177, 159);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(459, 214);
+            dataGridView1.TabIndex = 3;
+            // 
+            // FormPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnExcluir);
+            Controls.Add(btnEditar);
+            Controls.Add(btnAdicionar);
+            Name = "FormPrincipal";
+            Text = "FormPrincipal";
+            Load += FormPrincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnAdicionar;
+        private Button btnEditar;
+        private Button btnExcluir;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ WinFormsApp17/FormCadastro.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WinFormsApp17
+{
+    partial class FormCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNome = new TextBox();
+            txtPreco = new TextBox();
+            btnSalvar = new Button();
+            lblNome = new Label();
+            lblPreco = new Label();
+            SuspendLayout();
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(218, 110);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(100, 23);
+            txtNome.TabIndex = 0;
+            // 
+            // txtPreco
+            // 
+            txtPreco.Location = new Point(218, 139);
+            txtPreco.Name = "txtPreco";
+            txtPreco.Size = new Size(100, 23);
+            txtPreco.TabIndex = 1;
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Location = new Point(219, 179);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(75, 23);
+            btnSalvar.TabIndex = 2;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.UseVisualStyleBackColor = true;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(151, 110);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(43, 15);
+            lblNome.TabIndex = 3;
+            lblNome.Text = "Nome:";
+            // 
+            // lblPreco
+            // 
+            lblPreco.AutoSize = true;
+            lblPreco.Location = new Point(151, 147);
+            lblPreco.Name = "lblPreco";
+            lblPreco.Size = new Size(40, 15);
+            lblPreco.TabIndex = 4;
+            lblPreco.Text = "Preco:";
+            // 
+            // FormCadastro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblPreco);
+            Controls.Add(lblNome);
+            Controls.Add(btnSalvar);
+            Controls.Add(txtPreco);
+            Controls.Add(txtNome);
+            Name = "FormCadastro";
+            Text = "FormCadastro";
+            Load += FormCadastro_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNome;
+        private TextBox txtPreco;
+        private Button btnSalvar;
+        private Label lblNome;
+        private Label lblPreco;
+    }
+}
